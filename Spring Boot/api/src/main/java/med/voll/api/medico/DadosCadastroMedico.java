a27d79892983ae4dd8e1bc22de5c1aaffb260e89
Gustavo <132701636+ByGustavoo@users.noreply.github.com>
2024-02-07 22:58:35 -0300

    Add files via upload

---
+++ Spring Boot/api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,32 @@
+package med.voll.api.medico;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+import org.springframework.validation.annotation.Validated;
+
+public record DadosCadastroMedico(
+
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String telefone,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+
+        @NotNull
+        Especialidade especialidade,
+
+        @NotNull
+        @Validated
+        DadosEndereco endereco) {
+}
